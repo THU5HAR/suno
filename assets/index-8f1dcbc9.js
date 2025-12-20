5ca1fe2a7807bea964a753d5bf81d0b2380a6881
THU5HAR <155169400+THU5HAR@users.noreply.github.com>
2025-12-20 07:27:29 +0000

    Deploying to gh-pages from @ THU5HAR/suno@d81f4b124c90ca2d0f22e96d819f3cb4d39d4700 🚀

--- assets/index-8f1dcbc9.js
+++
@@ -1,2 +0,0 @@
-import{a as e,F as p}from"./index-89a700e0.js";export{e as FFFSType,p as FFmpeg};
-//# sourceMappingURL=index-8f1dcbc9.js.map
---
+++ assets/index-b7bccebb.js
@@ -0,0 +1,2 @@
+import{a as e,F as p}from"./index-8ea89bb4.js";export{e as FFFSType,p as FFmpeg};
+//# sourceMappingURL=index-b7bccebb.js.map
